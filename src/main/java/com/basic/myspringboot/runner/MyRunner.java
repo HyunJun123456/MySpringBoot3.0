d6c9fb5351613a8bcd888f503a4dc18813fc1917
HYUNJUN123456 <juni303@chuniv.ac.kr>
2023-10-12 17:26:34 +0900

    message add

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -34,6 +34,7 @@ public class MyRunner implements ApplicationRunner {
         System.out.println("name = " + name);
         System.out.println("age = " + age);
         System.out.println("fullName = " + fullName);
+        System.out.println("git test");
     }
 
 }
